aeed2a110eea402c552b7a19ab4b4449a40f585f
Tobías Vega <tobiasvega1210@gmail.com>
2025-08-21 12:00:17 -0300

    feat: :sparkles: clases con constructores

---
+++ 08-clases-con-constructor.ts
@@ -0,0 +1,15 @@
+class Car {
+  make: string;
+
+  constructor(make: string) {
+    this.make = make;
+  }
+
+  drive(): void {
+    console.log(`Driving a ${this.make}`)
+  }
+}
+
+const car = new Car("Volvo");
+
+car.drive();
